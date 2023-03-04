7cd6ae29a703828ed7eb52d483e1d4ad27063e48
Yusuf Salih <yusufsalih4@gmail.com>
2023-03-04 08:58:25 +0530

    completing my 1st react-native application

--- App.js
+++ App.js
@@ -1,69 +1,9 @@
 // import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  TouchableNativeFeedback,
-  Text,
-  Image,
-  SafeAreaView,
-  TouchableOpacity,
-  View,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-} from "react-native";
+import React from "react";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  let x = 1;
-  console.log("App Executed");
-  const handlePress = () => console.log("text Clicked");
-  const imagetap = () => console.log("image Clicked");
-  const buttonclick = () => console.log("button Clicked");
-  const containerStyle = { backgroundColor: "orange" };
-  return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <TouchableNativeFeedback onPress={imagetap}>
-        {/* <Image
-          blurRadius={3}
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        /> */}
-        <View
-          style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
-        ></View>
-      </TouchableNativeFeedback>
-
-      {/* <image
-        source={{
-          width: 200,
-          height: 300,
-
-          uri: "https://picsum.photos/200/300",
-        }}
-      /> */}
-
-      <Button
-        color="orange"
-        onPress={() =>
-          Alert.prompt("My Title", "My Message", (text) => console.log(text))
-        }
-        title="Click Me"
-      />
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  // return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // justifyContent: "center",
-    // alignItems: "center",
-  },
-});
